84d4fc5fbdd00c30d67b8c85828999d2c32ad759
Chris Boeckel <cboe07@gmail.com>
2017-06-06 15:39:02 -0400

    Add bcrypt

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
-var config = require('../config/config')
+var config = require('../config/config');
+var bcrypt = require('bcrypt-nodejs');
+
 
 var mysql = require('mysql');
 var connection = mysql.createConnection({
@@ -27,9 +29,9 @@ router.get('/', function(req, res, next) {
 	// const apiKey = 'fec8b5ab27b292a68294261bb21b04a5';
 	request.get(nowPlayingUrl,(error, response, movieData)=>{
 		var movieData = JSON.parse(movieData);
-		console.log("===============");
-		console.log(req.session);
-		console.log("===============");
+		// console.log("===============");
+		// console.log(req.session);
+		// console.log("===============");
 		res.render('index', { 
 			movieData: movieData.results,
 			imageBaseUrl: imageBaseUrl,
@@ -57,7 +59,8 @@ router.post('/search', (req,res)=>{
 		res.render('index', {
 			movieData: movieData.results,
 			imageBaseUrl: imageBaseUrl,
-			titleHeader: `You search for ${termUserSearchedFor}`
+			titleHeader: `You search for ${termUserSearchedFor}`,
+			sessionInfo: req.session
 		});
 	});
 
@@ -108,13 +111,15 @@ router.post('/registerProcess', (req,res)=>{
 	var name = req.body.name;
 	var email = req.body.email;
 	var password = req.body.password;
+	var hash = bcrypt.hashSync(password);
+	console.log(hash);
 
 	var selectQuery = "SELECT * FROM users WHERE email = ?";
 	connection.query(selectQuery, [email],(error,results)=>{
 		if(results.length == 0){
 			// User is not in db. Insert them
 			var insertQuery = "INSERT INTO users (name,email,password) VALUES (?,?,?)";
-			connection.query(insertQuery, [name,email,password], (error, results)=>{
+			connection.query(insertQuery, [name,email,hash], (error, results)=>{
 				// Add session vars -- name, email, loggedin, id
 				req.session.name = name;
 				req.session.email = email;
@@ -135,6 +140,38 @@ router.get('/login', (req,res)=>{
 	// res.send("This is the login page.")
 	res.render('login',{ });
 });
+
+router.post('/processLogin', (req,res)=>{
+	// res.json(req.body);
+	var email = req.body.email;
+	var password = req.body.password;
+	// var selectQuery = "SELECT * FROM users WHERE email = ? AND password = ?";
+	var selectQuery = "SELECT * FROM users WHERE email = ?";
+	connection.query(selectQuery,[email], (error,results)=>{
+		if(results.length == 1){
+			// Match found!
+			// Check to see if password matches
+
+			var match = bcrypt.compareSync(password, results[0].password);
+			if(match === true){
+				// We passed the english password and hash through compareSync, and they did match
+				req.session.loggedin = true;
+				req.session.name = results[0].name;
+				req.session.email = results[0].email;
+				res.redirect('/?msg=loggedin');
+			}else{
+				res.redirect('/login?msg=badLogin');
+			}
+
+
+			
+		}else{
+			// This isnt the droid we're looking for
+			res.redirect('/login?msg=badLogin');
+		}
+	});
+});
+
 
 
 
